8f10d6e3101722cd1fd829c2610c59677535a111
Kevin Williams <kjoewill@gmail.com>
2025-06-25 16:47:25 -0600

    Removed webserver from config since it is not used

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -3,20 +3,6 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   timeout: 30_000,
-  webServer: [
-    {
-      command: './run_uvicorn.sh',
-      cwd: '../backend',
-      port: 8000,
-      reuseExistingServer: !process.env.CI,
-    },
-    {
-      command: 'npm run dev -- --port 5173 --strictPort',
-      cwd: '.',
-      port: 5173,
-      reuseExistingServer: !process.env.CI,
-    },
-  ],
   use: {
     baseURL: 'http://localhost:5173',
   },
