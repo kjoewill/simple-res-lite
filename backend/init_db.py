446cbc9d1185c04b60485d8c789d210d8f693e65
Kevin Williams <kjoewill@gmail.com>
2025-06-27 17:28:53 -0600

    🛠️ Supporting path to schema.sql in all three environments

--- backend/init_db.py
+++ backend/init_db.py
@@ -1,8 +1,14 @@
+import os
 import sqlite3
 
 def init_db():
-    with sqlite3.connect("database.db") as conn:
-        with open("backend/schema.sql", "r") as f:
+    # Always resolves path relative to this file's location
+    base_dir = os.path.dirname(__file__)  # -> backend/
+    schema_path = os.path.join(base_dir, "schema.sql")
+    db_path = os.path.join(base_dir, "database.db")
+
+    with sqlite3.connect(db_path) as conn:
+        with open(schema_path, "r") as f:
             conn.executescript(f.read())
 
 if __name__ == "__main__":
