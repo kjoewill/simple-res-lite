1e243faccd2f6ea25a69e84304a46d7316922174
Kevin Williams <kjoewill@gmail.com>
2025-07-01 16:10:13 -0600

    ensure relative path for deployed statis assets on fly

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './', // ✅ ensures relative paths for deployed static assets
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://127.0.0.1:8000', // forward API requests to FastAPI
+      '/api': 'http://127.0.0.1:8000',
     },
   },
 })
